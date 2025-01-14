8997ebc3a3de6172349f919085a1fcca6da4cef1
JackooDesu <45838705+JacKooDesu@users.noreply.github.com>
2025-01-14 13:51:33 +0800

    update ts project structure

--- TsProject/post-copy.js
+++ TsProject/post-copy.js
@@ -1,13 +1,13 @@
 var copyfiles = require("copyfiles");
 
 const destMap = {
-  win: "../build/StandaloneWindows64/mission-import",
-  linux: "../build/StandaloneLinux64/mission-import",
+  win: "../build/StandaloneWindows64",
+  linux: "../build/StandaloneLinux64",
 };
 const dest = destMap[process.argv[2]] || "./output/";
 
 copyfiles(
-  ["src/**/*", dest],
+  ["src/missions/**/*", dest],
   { up: 1, verbose: true, exclude: ["src/**/*.*ts"] },
   function (err) {
     if (err) {
